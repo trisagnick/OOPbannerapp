51e6e5fd0b3f33fe05f44181d47b6a1bf7dfc23c
trisagnick <trisagnickchandra@gmail.com>
2026-02-23 08:32:30 +0530

    UC3 Done

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+        System.out.println(String.join("   ", " *** ", " *** ", "*****", "*****"));
+        System.out.println(String.join("   ", "*   *", "*   *", "*   *", "*    "));
+        System.out.println(String.join("   ", "*   *", "*   *", "*   *", "*    "));
+        System.out.println(String.join("   ", "*   *", "*   *", "*****", "*****"));
+        System.out.println(String.join("   ", "*   *", "*   *", "*    ", "    *"));
+        System.out.println(String.join("   ", "*   *", "*   *", "*    ", "    *"));
+        System.out.println(String.join("   ", "*   *", "*   *", "*    ", "*   *"));
+        System.out.println(String.join("   ", " *** ", " *** ", "*    ", "*****"));
+
+    }
+}
