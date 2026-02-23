86b8fe3df01e3d89e42f90a0d14a4d7ed63d4374
trisagnick <trisagnickchandra@gmail.com>
2026-02-23 08:28:31 +0530

    UC2 Done

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class OOPSBannerApp
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println(" ***** "+" "+" ***** "+" "+"****** "+" "+" ***** ");
+        System.out.println("*     *"+" "+"*     *"+" "+"*     *"+" "+"*     *");
+        System.out.println("*     *"+" "+"*     *"+" "+"*     *"+" "+"*      ");
+        System.out.println("*     *"+" "+"*     *"+" "+"****** "+" "+" ***** ");
+        System.out.println("*     *"+" "+"*     *"+" "+"*      "+" "+"      *");
+        System.out.println("*     *"+" "+"*     *"+" "+"*      "+" "+"      *");
+        System.out.println(" ***** "+" "+" ***** "+" "+"*      "+" "+"****** ");
+    }
+}
