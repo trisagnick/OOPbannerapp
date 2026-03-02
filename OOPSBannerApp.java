82a34b6fa380866a4c285e0c6857383b384e7baf
trisagnick <trisagnickchandra@gmail.com>
2026-03-02 08:24:44 +0530

    UC5 Done

---
+++ OOPSBannerApp.java
@@ -0,0 +1,19 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+            " ***     ***    *****    ***** ",
+            "*   *   *   *   *    *   *     ",
+            "*   *   *   *   *    *   *     ",
+            "*   *   *   *   *****     ***  ",
+            "*   *   *   *   *            * ",
+            "*   *   *   *   *            * ",
+            " ***     ***    *        ***** "
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
